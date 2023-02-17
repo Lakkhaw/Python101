dec476fd7ffbece439dd834e2e5a6c8897daa7a1
Lakkhaw <113591735+Lakkhaw@users.noreply.github.com>
2023-02-17 12:36:44 +0700

    โปรแกรมที่ทำการบ้านเสร็จ

---
+++ GUI.knowledge.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+GUI = Tk() # นี้คือหน้าจอหลักของโปรแกรม
+GUI.title('โปรแกรมบันทึดข้อมูล')
+GUI.geometry('500x400')
+
+B1 = Button(GUI,text='เงินมีกี่บาท')
+B1.pack()
+
+
+
+
+GUI=mainloop()
